bb9049e4d0f7fa131364c01fc747561cfc3fc523
PIXELL07 <arunksn007@gmail.com>
2026-02-28 00:39:02 +0530

    feat: define get func

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,23 @@
+// Private application code
+
 package config
 
-// Private application code
+type Config struct {
+	PostgresDSN string
+	RedisAddr   string
+	MinioURL    string
+	MinioKey    string
+	MinioSecret string
+	Bucket      string
+}
+
+func Load() Config {
+	return Config{
+		PostgresDSN: get("POSTGRES_DSN", "postgres://postgres:postgres@postgres:5432/docflow?sslmode=disable"),
+		RedisAddr:   get("REDIS_ADDR", "redis:6379"),
+		MinioURL:    get("MINIO_URL", "minio:9000"),
+		MinioKey:    get("MINIO_ROOT_USER", "minioadmin"),
+		MinioSecret: get("MINIO_ROOT_PASSWORD", "minioadmin"),
+		Bucket:      get("MINIO_BUCKET", "uploads"),
+	}
+}
