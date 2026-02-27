4f8cae1d5a57d2d85d363cdde8f889872248dfbc
PIXELL07 <arunksn007@gmail.com>
2026-02-28 00:46:09 +0530

    feat: db initial,tbi import lib/pq

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,1 +1,25 @@
 package db
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func New(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(30)
+	db.SetMaxIdleConns(30)
+	db.SetConnMaxLifetime(10 * time.Minute)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	return db, db.PingContext(ctx)
+}
