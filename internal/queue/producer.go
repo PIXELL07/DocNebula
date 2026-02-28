a2b689619c607b71bc1d12d1dc57f5dc4a8ea534
PIXELL07 <arunksn007@gmail.com>
2026-03-01 01:15:52 +0530

    feat: commit undefined Message fixed, fix redis import to go.mod file..

--- internal/queue/producer.go
+++ internal/queue/producer.go
@@ -1,1 +1,27 @@
 package queue
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type Message struct {
+	JobID     string    `json:"job_id"`
+	Attempt   int       `json:"attempt"`
+	Timestamp time.Time `json:"ts"`
+}
+
+type Producer struct {
+	Client *redis.Client
+}
+
+func (p *Producer) Publish(ctx context.Context, queue string, msg Message) error {
+	b, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	return p.Client.LPush(ctx, queue, b).Err()
+}
