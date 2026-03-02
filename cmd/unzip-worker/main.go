f07bee729c6433942c2443f926430971257e6e4b
PIXELL07 <arunksn007@gmail.com>
2026-03-03 02:22:50 +0530

    feat: handler publish nxt at ocr - vector queue

--- cmd/unzip-worker/main.go
+++ cmd/unzip-worker/main.go
@@ -1,1 +1,38 @@
-package unzipworker
+package main
+
+import (
+	"DocNebula/internal/queue"
+	"context"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	ctx := context.Background()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	rdb := redis.NewClient(&redis.Options{Addr: "redis:6379"})
+
+	consumer := &queue.Consumer{
+		Client:      rdb,
+		DLQ:         "unzip_dlq",
+		WorkerCount: 4,
+		Logger:      logger,
+	}
+	producer := &queue.Producer{Client: rdb}
+
+	handler := func(ctx context.Context, msg queue.Message) error {
+		logger.Info("UNZIP start", "job_id", msg.JobID)
+
+		// TODO: real unzip work
+		time.Sleep(500 * time.Millisecond)
+
+		return producer.Publish(ctx, "ocr_queue", msg)
+	}
+
+	consumer.Start(ctx, "unzip_queue", handler, producer)
+}
