207454590c193f00fffd4add57e92ef5f44db261
PIXELL07 <arunksn007@gmail.com>
2026-03-04 00:06:46 +0530

    feat: ocr-worker

--- cmd/ocr-worker/main.go
+++ cmd/ocr-worker/main.go
@@ -1,1 +1,37 @@
-package ocrworker
+package main
+
+import (
+	"DocNebula/internal/queue"
+	"context"
+	"log/slog"
+	"os"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	ctx := context.Background()
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	rdb := redis.NewClient(&redis.Options{Addr: "redis:6379"})
+
+	consumer := &queue.Consumer{
+		Client:      rdb,
+		DLQ:         "ocr_dlq",
+		WorkerCount: 4,
+		Logger:      logger,
+	}
+	producer := &queue.Producer{Client: rdb}
+
+	handler := func(ctx context.Context, msg queue.Message) error {
+		logger.Info("OCR start", "job_id", msg.JobID)
+
+		// simulate OCR
+		time.Sleep(700 * time.Millisecond)
+
+		return producer.Publish(ctx, "vector_queue", msg)
+	}
+
+	consumer.Start(ctx, "ocr_queue", handler, producer)
+}
