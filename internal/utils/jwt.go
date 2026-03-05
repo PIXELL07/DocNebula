31b1bf613ea0bd990cf18c19fef9177e98f2f366
PIXELL07 <arunksn007@gmail.com>
2026-03-05 18:42:15 +0530

    fix: import jwt for signing

---
+++ internal/utils/jwt.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var Secret = []byte("DocNebula-secret")
+
+func GenerateToken(userID string) (string, error) {
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	})
+
+	return token.SignedString(Secret)
+}
