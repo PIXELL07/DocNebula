1ff8920c2f82de2f4abc27c8571f8df9741a53db
PIXELL07 <arunksn007@gmail.com>
2026-03-06 01:10:31 +0530

    feat: commit

--- internal/db/migrations/002_users.sql
+++ internal/db/migrations/002_users.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
     id TEXT PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at NOT TIMESTAMP DEFAULT NOW()
 );
