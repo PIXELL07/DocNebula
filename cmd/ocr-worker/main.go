91bbc5ded794344b4e326104aa26045e90d87c70
PIXELL07 <arunksn007@gmail.com>
2026-03-06 01:23:47 +0530

    feat: commit

--- cmd/ocr-worker/main.go
+++ cmd/ocr-worker/main.go
@@ -16,10 +16,26 @@ import (
 )
 
 func main() {
+
 	ctx := context.Background()
+
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	rdb := redis.NewClient(&redis.Options{Addr: "redis:6379"})
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = "localhost:6379"
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	})
+
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		logger.Error("redis connection failed", "err", err)
+		os.Exit(1)
+	}
+
+	logger.Info("ocr worker started")
 
 	consumer := &queue.Consumer{
 		Client:      rdb,
@@ -27,13 +43,22 @@ func main() {
 		WorkerCount: 4,
 		Logger:      logger,
 	}
+
 	producer := &queue.Producer{Client: rdb}
 
 	handler := func(ctx context.Context, msg queue.Message) error {
-		logger.Info("OCR start", "job_id", msg.JobID)
 
-		// simulate OCR
+		logger.Info("OCR start",
+			"job_id", msg.JobID,
+			"attempt", msg.Attempt,
+		)
+
+		// TODO: replace with real OCR engine
 		time.Sleep(700 * time.Millisecond)
+
+		logger.Info("OCR finished",
+			"job_id", msg.JobID,
+		)
 
 		return producer.Publish(ctx, "vector_queue", msg)
 	}
--- cmd/unzip-worker/main.go
+++ cmd/unzip-worker/main.go
@@ -11,11 +11,26 @@ import (
 )
 
 func main() {
+
 	ctx := context.Background()
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	rdb := redis.NewClient(&redis.Options{Addr: "redis:6379"})
+	redisAddr := os.Getenv("REDIS_ADDR")
+	if redisAddr == "" {
+		redisAddr = "localhost:6379"
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisAddr,
+	})
+
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		logger.Error("redis connection failed", "err", err)
+		os.Exit(1)
+	}
+
+	logger.Info("unzip worker started")
 
 	consumer := &queue.Consumer{
 		Client:      rdb,
@@ -23,13 +38,22 @@ func main() {
 		WorkerCount: 4,
 		Logger:      logger,
 	}
+
 	producer := &queue.Producer{Client: rdb}
 
 	handler := func(ctx context.Context, msg queue.Message) error {
-		logger.Info("UNZIP start", "job_id", msg.JobID)
 
-		// TODO: real unzip work
+		logger.Info("UNZIP start",
+			"job_id", msg.JobID,
+			"attempt", msg.Attempt,
+		)
+
+		// TODO: real unzip logic
 		time.Sleep(500 * time.Millisecond)
+
+		logger.Info("UNZIP finished",
+			"job_id", msg.JobID,
+		)
 
 		return producer.Publish(ctx, "ocr_queue", msg)
 	}
